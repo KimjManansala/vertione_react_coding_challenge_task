a63b0015c995645fde59493d950824b7b48689b7
KimJ Manansala <kimjmanansala@gmail.com>
2021-07-31 11:30:08 -0400

    Fine tuned some of the logic

--- front_end/src/OrderList.jsx
+++ front_end/src/OrderList.jsx
@@ -15,36 +15,81 @@ const Emoji = ({ label, symbol }) => (
 
 function OrderList(props) {
 
+    const [inputValue, setInputValue] = useState('')
     const [itemList, setItemList] = useState([])
-    const [isListDescending, setIsListDescending] = useState(true)
+    const [isListDescending, setIsListDescending] = useState(false)
+
+    const sortItemList = (list, descending = false) => {
+        // Note function is expected to mutate given list
+        list.sort()
+        if (descending) {
+            list.reverse()
+        }
+    }
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value)
+    }
 
     const listenKeydownEvent = (event) => {
-        console.log('event', event)
+        if (event.key === 'Enter') {
+            const newItemList = itemList.concat([inputValue])
+            sortItemList(newItemList, isListDescending)
+            setItemList(newItemList)
+            setInputValue('')
+        }
+
+    }
+
+    const handleDirectionClick = () => {
+        const newItemList = [...itemList]
+        sortItemList(newItemList , !isListDescending)
+        setIsListDescending(!isListDescending)
+        setItemList(newItemList)
+    }
+
+    const handleClearList = () => {
+        setItemList([])
     }
 
     return (
-        <div>
-            <div className="button-group">
+        <div className="container">
+            <div className="input-group">
                 <input
+                    onChange={handleInputChange}
                     onKeyDown={listenKeydownEvent}
+                    value={inputValue}
                     className="add-item"
                 >
                 </input>
-                <button class="sort-direction">
+                <button
+                    className="sort-direction"
+                    onClick={handleDirectionClick}
+                >
                     {isListDescending ? (
-                        <Emoji
-                            label="down_arrow"
-                            symbol="⬇️"
-                        />
-                    ) : (
                         <Emoji
                             label="up_arrow"
                             symbol="⬆️"
                         />
+                    ) : (
+                        <Emoji
+                            label="down_arrow"
+                            symbol="⬇️"
+                        />
                     )}
+                </button>
+                <button
+                    className="clear-list"
+                    onClick={handleClearList}
+                >
+                    <Emoji
+                        label="clear"
+                        symbol="❌"
+                    />
                 </button>
             </div>
             <ItemList
+                itemList={itemList}
             />
         </div>
     );
